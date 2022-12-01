f08670b1c9d7fd975ec4ae7fc754886c00354bad
Gabriel Cassanta <cassantagabriel@gmail.com>
2022-12-01 08:56:21 -0300

    feat: fetchData from RAPID_API

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
+const selectedCategory = "New";
+
 const Sidebar = () => (
   <Stack
     direction="row"
@@ -11,7 +13,30 @@ const Sidebar = () => (
       flexDirection: { md: "column" },
     }}
   >
-    Sidebar
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
   </Stack>
 );
 
