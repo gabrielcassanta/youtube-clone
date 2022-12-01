950f4a7b46f43cf54160c2835b09c15633976ae6
Gabriel Cassanta <cassantagabriel@gmail.com>
2022-12-01 11:29:46 -0300

    feat: videoCard added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Box, Stack } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => {
-  return <div>Videos</div>;
+const Videos = ({ videos }) => {
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>
+      ))}
+    </Stack>
+  );
 };
 
 export default Videos;
